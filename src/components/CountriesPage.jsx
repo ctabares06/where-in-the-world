9eee8fc95a3368a55479b546fdfbc2321a700917
ctabares06 <ctr9120@gmail.com>
2021-08-29 01:28:37 -0500

    countries page styles

--- src/components/CountriesPage.jsx
+++ src/components/CountriesPage.jsx
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import Countries from './Countries';
 import SearchCountry from './SearchCountry';
+
+const PageStyles = styled.div`
+	display: grid;	
+	grid-template-columns: 1fr;
+	grid-template-rows: [filters-start] 20% [filters-end countries-start] auto [countries-end];
+	row-gap: 50px;
+	align-items: center;
+`
 
 class CountriesPage extends Component {
 	render() {
 		return (
-			<>
+			<PageStyles>
 				<SearchCountry />
 				<Countries />
-			</>
+			</PageStyles>
 		)
 	}
 }
--- src/styles/utils.styles.js
+++ src/styles/utils.styles.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
 
 export const withShadow = (Component) => styled(Component)`
-  box-shadow: 0px 3px 5px 1px rgba(152,152,152,0.74);
+  box-shadow: 0px 3px 5px 1px rgba(152,152,152,0.2);
 `
